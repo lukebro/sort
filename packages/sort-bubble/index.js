8ebb01e46ce952b6bc344847442e56a173c40ec6
Lukasz Brodowski <lukebrodowski@gmail.com>
2016-07-12 19:14:14 -0400

    added bubble sort

---
+++ packages/sort-bubble/index.js
@@ -0,0 +1,28 @@
+/**
+ * Sort an array via bubble sort
+ * 
+ * @param  {Array} arr - The array to be sorted.
+ * @param  {Function} comparator - Determines how to compare values in the array.
+ * @return {Array} - The sorted array.
+ */
+function bubbleSort(arr, comparator) {
+	comparator = comparator == null ? function (a, b) { return a > b } : comparator
+	var n = arr.length
+
+	while (n != 0) {
+		var k = 0
+		for (var i = 1; i < n; i++) {
+			if (comparator(arr[i - 1], arr[i])) {
+				var temp = arr[i - 1]
+				arr[i - 1] = arr[i]
+				arr[i] = temp
+				k = i
+			}
+		}
+		n = k
+	}
+
+	return arr
+}
+
+module.exports = bubbleSort
