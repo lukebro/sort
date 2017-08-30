af668dd13ae53793383fcbf089a0f3ebd3b69337
Lukasz Brodowski <lukebrodowski@gmail.com>
2017-08-30 18:31:32 -0400

    replaced yarn with ava  🔮

--- test/index.js
+++ test/index.js
@@ -1,39 +1,31 @@
-var test = require('tape')
-var sorts = {
-	insertion: require('../packages/sort-insertion'),
-	bubble: require('../packages/sort-bubble'),
-	selection: require('../packages/sort-selection'),
-}
+import test from 'ava';
 
-Object.keys(sorts).forEach(function (key) {
-   var sort = sorts[key]
+let sorts = new Map([
+	['insertion', require('../packages/sort-insertion')],
+	['bubble', require('../packages/sort-bubble')],
+	['selection', require('../packages/sort-selection')],
+]);
 
-	test(key + ': sorts an array of numbers with comparator passed', function (t) {
-		var arr = [6, 2, 5, 10, 23, 1, 0]
-		t.deepEqual(sort(arr, function (a, b) { return a > b }), [0, 1, 2, 5, 6, 10, 23])
+sorts.forEach((sort, name) => {
 
-		t.end()
-	})
+	test(`{name}: sorts an array of numbers with comparator passed`, t => {
+		let arr = [6, 2, 5, 10, 23, 1, 0];
+		t.deepEqual(sort(arr, (a, b) => a > b), [0, 1, 2, 5, 6, 10, 23]);
+	});
 
-	test(key + ': sorts an array of numbers without comparator passed', function (t) {
-		var arr = [6, 2, 5, 10, 23, 1, 0]
-		t.deepEqual(sort(arr), [0, 1, 2, 5, 6, 10, 23])
+	test(`{name}: sorts an array of numbers without comparator passed`, t => {
+		let arr = [6, 2, 5, 10, 23, 1, 0];
+		t.deepEqual(sort(arr), [0, 1, 2, 5, 6, 10, 23]);
+	});
 
-		t.end()
-	})
+	test(`{name}: sorts an array of objects`, t => {
+		let arr = [{ value: 30 }, { value: 32 }, { value: 40 }, { value: 10 }];
+		t.deepEqual(sort(arr, (a, b) => a.value > b.value), [{ value: 10 }, { value: 30 }, { value: 32 }, { value: 40 }]);
+	});
 
-	test(key + ': sorts an array of objects', function (t) {
-		var arr = [{ value: 30 }, { value: 32 }, { value: 40 }, { value: 10 }]
-		t.deepEqual(sort(arr, function (a, b) { return a.value > b.value }), [{ value: 10 }, { value: 30 }, { value: 32 }, { value: 40 }])
+	test(`{name}: sorting an empty array returns an empty array`, t => {
+		let arr = [];
+		t.deepEqual(sort(arr), []);
+	});
 
-		t.end()
-	})
-
-	test(key + ': sorting an empty array returns an empty array', function (t) {
-		var arr = []
-		t.deepEqual(sort(arr), [])
-
-		t.end()
-	})
-
-})
+});
